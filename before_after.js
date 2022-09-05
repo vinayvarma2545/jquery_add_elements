b036435a07b491da6ebb44df405b5ae108fc26fd
vinay <vinayvarma2545@gmail.com>
2022-09-05 19:52:39 +0530

    add_elements

---
+++ before_after.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("#b4").click(function(){
+        $("p").before("<i>Hellow </i>");
+    });
+
+    $("#after").click(function(){
+        $("p").after("<i>im vinay</i>")
+    });
+
+});
